7d06a885964df3445b58de7381a28829886862e6
David Gao <davidgjm@gmail.com>
2018-06-25 09:16:07 +0800

    changed configuration to factory bean style

---
+++ src/main/java/guru/springframework/services/GreetingRepositoryImpl.java
@@ -0,0 +1,21 @@
+package guru.springframework.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingRepositoryImpl implements GreetingRepository {
+    @Override
+    public String getEnglishGreeting() {
+        return "Hi there -- Primary Greeting Service";
+    }
+
+    @Override
+    public String getSpanishGreeting() {
+        return "Servicio de Saludo Primario";
+    }
+
+    @Override
+    public String getGermanGreeting() {
+        return "Primärer Grußdienst";
+    }
+}
--- src/main/java/guru/springframework/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/services/GreetingServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingServiceImpl implements GreetingService {
     private static final String MESSAGE = "Hello there! from greetingService -- original";
+
     @Override
     public String sayHello() {
         return MESSAGE;
